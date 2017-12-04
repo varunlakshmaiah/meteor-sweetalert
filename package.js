ad1c4040127818569171067bb848ec60e3764c25
Arun Kumar L <arunkumarl@whizchip.com>
2017-12-04 15:41:06 +0530

    Modified Package.js

--- package.js
+++ package.js
@@ -2,23 +2,19 @@ Package.describe({
   name: 'varunlakshmaiah:sweetalert',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'SweetAlert Integration',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/varunlakshmaiah/meteor-sweetalert.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.6');
-  api.use('ecmascript');
-  api.mainModule('sweetalert.js');
-});
+  api.versionsFrom('1.3.2');  
+     api.add_files([
+        'sweetalert/src/sweetalert.css',
+        'sweetalert/src/sweetalert.js'
+    ], ['client']);
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('varunlakshmaiah:sweetalert');
-  api.mainModule('sweetalert-tests.js');
 });
--- sweetalert.js
+++
@@ -1,5 +0,0 @@
-// Write your package code here!
-
-// Variables exported by this module can be imported by other packages and
-// applications. See sweetalert-tests.js for an example of importing.
-export const name = 'sweetalert';
